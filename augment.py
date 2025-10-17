a4f56b79d6b05bb937bc4a425b3e514f98aa7ebf
Illy Hoang <133312266+illydh@users.noreply.github.com>
2025-10-17 15:07:18 -0700

    restructuring

--- augment.py
+++ augment.py
@@ -1,22 +1,26 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image_dataset_from_directory
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from tensorflow.keras.preprocessing.image import (
+    ImageDataGenerator,
+    img_to_array,
+    load_img,
+)
 import numpy as np
 import pickle
 
 
-print(tf.config.list_physical_devices('GPU'))
+print(tf.config.list_physical_devices("GPU"))
 
 # Path to your main folder
-data_dir = "data/resized"
+data_dir = "data/testset"
 
 # Create dataset
 dataset = image_dataset_from_directory(
     data_dir,
     image_size=(128, 128),  # resize all images
-    batch_size=None,        # if you want to get all images in memory
-    label_mode="int",        # or "categorical" / "binary" / None
-    color_mode="grayscale"
+    batch_size=None,  # if you want to get all images in memory
+    label_mode="int",  # or "categorical" / "binary" / None
+    color_mode="grayscale",
 )
 
 # Extract X (images) and y (labels)
@@ -34,13 +38,13 @@ print(X.shape, y.shape)
 
 # Create an ImageDataGenerator with desired augmentations
 datagen = ImageDataGenerator(
-    rotation_range=30,       # random rotation
-    width_shift_range=0.1,   # horizontal shift
+    rotation_range=30,  # random rotation
+    width_shift_range=0.1,  # horizontal shift
     height_shift_range=0.1,  # vertical shift
-    zoom_range=0.2,          # zoom in/out
-    shear_range=10,          # shearing
-    horizontal_flip=True,    # random horizontal flip
-    fill_mode='nearest'
+    zoom_range=0.2,  # zoom in/out
+    shear_range=10,  # shearing
+    horizontal_flip=True,  # random horizontal flip
+    fill_mode="nearest",
 )
 
 # Lists to store augmented images and labels
@@ -61,7 +65,7 @@ for i in range(len(X)):
 X_aug = np.array(X_aug)
 y_aug = np.array(y_aug)
 
-with open('data/X_aug_y_aug.pkl', 'wb') as file:
-        pickle.dump((X_aug, y_aug), file)
+with open("data/X_aug_y_aug.pkl", "wb") as file:
+    pickle.dump((X_aug, y_aug), file)
 
 # print("Original:", X.shape, "→ Augmented:", X_aug.shape)
