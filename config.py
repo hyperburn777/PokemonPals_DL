f2ba7938e3dc263f000867cb6399bb5f4b1d6d2f
Joseph Yue <joseph.yue2012@gmail.com>
2025-10-22 10:52:08 -0700

    removed validation set and trying simple model

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ RESULT_DIR = os.getenv("RESULT_DIR", "result")
 IMG_SIZE = (128, 128)
 CHANNELS = 1
 BATCH = 64
-VAL_SPLIT  = 0.125 # 12% of training for validation
+VAL_SPLIT  = 0.0 # 12% of training for validation
 
 # Training
 EPOCHS = 60
 INIT_LR = 3e-4
 WEIGHT_DECAY = 1e-4
 LABEL_SMOOTH = 0.05
-BACKBONE = os.getenv("BACKBONE", "silhouette")  # or "silhouette"
+BACKBONE = os.getenv("BACKBONE", "simple")  # or "silhouette"
 
 # EfficientNet fine-tuning depth (0 = freeze all, higher = unfreeze more)
 TRAINABLE_AT = int(os.getenv("TRAINABLE_AT", "200"))
